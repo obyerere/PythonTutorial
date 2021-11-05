537ad02c29a8bc821af5c9eb8b45f2449c513725
Oby <oechuks@gmail.com>
2021-11-05 14:58:26 -0400

    Added Conditional.py for if elif for n while

---
+++ conditionals.py
@@ -0,0 +1,24 @@
+#LAB1
+fav_linux_distros = ['Mint', 'Debian', 'Ubuntu', 'Manjaro', 'Fedora', 'Arch']
+
+for distro in fav_linux_distros:
+    print(distro)
+"""
+if "string 1" == "string 1":
+    print("These strings are equal.")
+
+if "string 1" != "string_2":
+    print("String 1 does not equal string 2.")
+
+if 2 < 4:
+    print("The first number is less than the second number.")
+
+if 4 > 2:
+    print("The first number is greater than the second number.")
+
+if 4 <= 4:
+    print("The first number is less than or equal to the second number.")
+
+if 4 >= 4:
+    print("The first number is greater than or equal to the second number.")
+"""
