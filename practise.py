feefd2534576327422dc9e1e076e20e325796d05
Oby <oechuks@gmail.com>
2021-11-05 23:58:05 -0400

    exception py

--- practise.py
+++ practise.py
@@ -1,3 +1,26 @@
+print('Please give me 2 numbers and I will divide them.')
+print('Enter "q" to quit.\n')
+    
+while True:
+    first_number = input('First Number: ')
+    if first_number == 'q':
+        break
+    
+    second_number = input('Second Number: ')
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print('You can\'t divide by zero! Let\'s start again.')
+    else:
+        print('Your answer is: ' + str(answer))
+        print('Give me another!\n')
+    
+print('Ok, bye!')
+
+
+'''
 # This is my section for list & dictionary
 my_week = [
     {'name':['Mary','John','Princess','Kayode']}, # Name Dictionary {'key':'value'} --> value in this case is a list of names 
@@ -6,3 +29,5 @@ my_week = [
     {'lesson':['French','Forensics','Music','Maths']} # Lesson Dictionary 
 ]
 print('This School week: '+'\n\t'+ str(my_week[0]['name'][2])+ '\n\t'+ str(my_week[1]['day'][0])+'\n\t'+ str(my_week[2]['activity'][2])+'\n\t'+ str(my_week[3]['lesson'][3]) )
+
+'''
