6ecd867e72d533eb6eca4dc186c94b3e429e07ab
Oby <oechuks@gmail.com>
2021-11-05 23:01:43 -0400

    Some more python

--- input.py
+++ input.py
@@ -1,3 +1,41 @@
+import random
+import sys
+
+# Pick a random word from a provided list
+def pick_random_word(list):
+    return random.choice(list)
+
+# Get a code name made up of the number of words specified
+def get_code_name(list, num_words):
+    if num_words.isdigit() == False:
+        return 'Error: incorrect argument provided. You must provide an integer.'
+
+    num_words = int(num_words)
+    code_name = ''
+
+    for x in range(1,num_words+1):
+        word = pick_random_word(list)
+        code_name += word + ' '
+
+    return code_name.rstrip()
+
+# List of words to use
+word_list = ['Aurora', 'Avalanche', 'Blizzard', 'Cyclone', 'Eagle', 'Edison', 'Frost', 'Hawk', 'Hexagon', 'Hornet', 'Medusa', 'Neptune', 'Orion', 'Osprey', 'Plato', 'Portal', 'Raven', 'Sand', 'Shadow', 'Storm', 'Sunset', 'Thunder', 'Vector', 'Vista', 'Vortex', 'Volcano']
+
+if len(sys.argv) > 1:
+    # Retrieve the command line argument
+    words_to_pick = input ('Pick a nymber')
+    words_to_pick = sys.argv[1]
+
+    # Create a code name and print it to the screen
+    code_name = get_code_name(word_list, sys.argv[1])
+    print(code_name)
+else:
+    print('Error: You must provide the number of words as an argument.')
+
+
+
+"""
 # Checks if the user's answer can be used by the determineReply() 
 # function, and if it cannot provides an error.
 def get_reply(user_input):
@@ -23,7 +61,7 @@ user_coffee_input = input('How many cups of coffee have you had today? ')
 reply = get_reply(user_coffee_input)
 print(reply)
 
-"""
+
 # practising input() with conditional statements
 user_coffee_input = input('How many cups of coffee have you had today? ')
 user_coffee_input = int (user_coffee_input)
